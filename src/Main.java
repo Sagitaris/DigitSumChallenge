cf3c95d2396165da73af0be6b5c68a99af6550c7
Anna Adamik <quenthel5@gmail.com>
2023-03-14 09:11:22 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(sumDigits(-5));
+        System.out.println(sumDigits(5));
+        System.out.println(sumDigits(125));
+        System.out.println(sumDigits(1000));
+        System.out.println(sumDigits(1234));
+        System.out.println(sumDigits(-125));
+        System.out.println(sumDigits(4));
+        System.out.println(sumDigits(32123));
+
+    }
+
+    public static int sumDigits(int number) {
+
+        if (number < 0) {
+            return -1;
+        }
+
+        int sum = 0;
+        while (number >= 1) {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        return sum;
+    }
+}
